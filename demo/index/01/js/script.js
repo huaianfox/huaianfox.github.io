8f31506d72222b8a9b2e0919d8c7117cb8d42d26
QLiang Zhang <zqingl@163.com>
2017-03-21 12:23:56 +0800

    ok

--- demo/index/01/js/script.js
+++ demo/index/01/js/script.js
@@ -1,6 +1,32 @@
 $(function () {
+    var timer=null,index=0;
+    timer=setInterval(function () {
+        auto();
+    },3000);
+    $(".slideTab span").mouseover(function () {
+        var index=$(this).index();
+        console.log(index);
+        play(index);
+    });
+    $(".slidebox").hover(function () {
+        clearInterval(timer);
+    },function () {
+        timer=setInterval(function () {
+            auto();
+        },3000)
+    });
+    function auto() {
+        index++;
+        (index>=4)&&(index=0);
+        play(index);
+    }
+    function play(cur) {
+        $(".slideTab span").eq(cur).addClass("active").siblings().removeClass("active");
+        $(".slide li").eq(cur).fadeIn(500).siblings().fadeOut(500);
+        index=cur;
+    }
+
     $(".on").hover(function () {
-        console.log($(this));
         $(this).find(".sub-menu").slideDown(500);
     },function () {
         $(this).find(".sub-menu").slideUp(500);
@@ -9,5 +35,4 @@ $(function () {
     $(".close").on("click",function () {
         $("#slideBar").fadeOut()
     });
-
 });
